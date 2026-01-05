4b3ead719454a67f1871d4ab7e348d85bcc6b935
FreshTart <imcgrady@andrew.cmu.edu>
2026-01-05 10:28:14 -0500

    Updated Recursion

---
+++ Python/Lesson 99 - Recursion/recursion_exercises.py
@@ -0,0 +1,60 @@
+# def factorial(n: int) -> int:
+#     if n == 1 or n == 0:
+#         pass
+
+
+# def re_sum_one(num_list: list[int]) -> int:
+#     print("Entering another recursion")
+#     if len(num_list) == 1:
+#         pass
+
+
+# max = 10
+# numbers = list(range(1, max + 1))
+# print(re_sum_one(numbers[:]))
+
+# def re_sum_half(num_list: list[int]) -> int:
+#     if len(num_list) == 1:
+#         pass
+
+# print(re_sum_half(numbers[:]))
+
+
+# def bad():
+#     return bad()
+
+# bad()
+
+
+# def fib(n: int) -> int:
+#     """
+#     0, 1, 2, 3, 4, 5, 6,  7,  8, ...
+#     0, 1, 1, 2, 3, 5, 8, 13, 21, ...
+#     """
+
+
+# print(fib(INT_HERE))
+
+
+# def binary_search(arr, low, high, x):
+#     """assumes array is already sorted"""
+#     if high >= low:
+#         pass
+
+
+# # Test array
+# arr = [2, 3, 4, 10, 40]
+# x = 10
+
+# # Function call
+# result = binary_search(arr, 0, len(arr) - 1, x)
+
+# if result != -1:
+#     pass
+
+
+# import os
+
+# def list_dir_recursive(directory):
+
+# list_dir_recursive('PATH_HERE')
